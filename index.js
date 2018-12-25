3e7c42d855c4d442b56b02d4798d18b709eca9a3
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-25 12:10:57 -0500

    Class 25

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // select svg container first
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+    .append('svg')
+        .attr('width', 600)
+        .attr('height', 600);
+
+// create margins and dimensions for the graph area
+const margin = {top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 d3.json('menu.js').then( data => {
-
     // const min = d3.min(data, d => d.orders);
     // const max = d3.max(data, d => d.orders);
     // const extent = d3.extent(data, d => d.orders);
@@ -17,7 +29,7 @@ d3.json('menu.js').then( data => {
         .paddingInner(0.2)
         .paddingOuter(0.2);
 
-    const rects = svg.selectAll('rect')
+    const rects = graph.selectAll('rect')
         .data(data);
 
     rects.attr('width', x.bandwidth)
