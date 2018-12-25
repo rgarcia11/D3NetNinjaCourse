6694dff8d84f145ce77510cec76cc512a7e1b494
Rogelio <r.garcia11@uniandes.edu.co>
2018-12-25 12:21:07 -0500

    Section/3 (#1)
    * Class 19
    
    * Class 20

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const data = [
-    {width: 200, height: 100, fill: 'purple'}
-];
-
+// select svg container first
 const svg = d3.select('svg');
 
-svg.select('rect')
-    .data(data)
-    .attr('width', d => d.width)
-    .attr('height', d => d.height)
-    .attr('fill', d => d.fill);
+d3.json("planets.json").then(data => {
+    const circs = svg.selectAll('circle')
+        .data(data);
+    
+    // add attrs to circs already in DOM (there are none, but its safer)
+    circs.attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d => d.radius)
+        .attr('fill', d => d.fill);
+
+    // append the enter selection to the DOM and add attributes
+    circs.enter()
+        .append('circle')
+            .attr('cy', 200)
+            .attr('cx', d => d.distance)
+            .attr('r', d => d.radius)
+            .attr('fill', d => d.fill);
+})
