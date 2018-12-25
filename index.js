2c92542770d6b2f84ae3b45456ef7f9a216b80b7
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-25 01:04:12 -0500

    Class 19

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const data = [
-    {width: 200, height: 100, fill: 'purple'}
+    {width: 200, height: 100, fill: 'purple'},
+    {width: 100, height: 60, fill: 'pink'},
+    {width: 50, height: 30, fill: 'red'}
 ];
 
 const svg = d3.select('svg');
 
-svg.select('rect')
-    .data(data)
-    .attr('width', d => d.width)
+const rect = svg.selectAll('rect')
+    .data(data)                     // join data
+    .attr('width', d => d.width)    // attrs to rects already in the DOM
+    .attr('height', d => d.height)
+    .attr('fill', d => d.fill)
+    .enter()                        // virtual rects
+    .append('rect')                 // add rects
+    .attr('width', d => d.width)    // attrs to new rects
     .attr('height', d => d.height)
     .attr('fill', d => d.fill);
