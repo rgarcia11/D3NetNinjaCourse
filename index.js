7ca4ccd7db06ffa9264a99ba99268159a3d7cd8b
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-25 01:43:30 -0500

    Class 22

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 // select svg container first
 const svg = d3.select('svg');
 
-d3.json("planets.json").then(data => {
-    const circs = svg.selectAll('circle')
-        .data(data);
-    
-    // add attrs to circs already in DOM (there are none, but its safer)
-    circs.attr('cy', 200)
-        .attr('cx', d => d.distance)
-        .attr('r', d => d.radius)
-        .attr('fill', d => d.fill);
+d3.json('menu.js').then( data => {
 
-    // append the enter selection to the DOM and add attributes
-    circs.enter()
-        .append('circle')
-            .attr('cy', 200)
-            .attr('cx', d => d.distance)
-            .attr('r', d => d.radius)
-            .attr('fill', d => d.fill);
-})
+    const y = d3.scaleLinear()
+        .domain([0, 1000])
+        .range([0, 500])
+
+    const rects = svg.selectAll('rect')
+        .data(data);
+
+    rects.attr('width', '50')
+        .attr('height', d => y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', (d, i) => i * 70 + 10);
+
+    rects.enter()
+        .append('rect')
+            .attr('width', '50')
+            .attr('height', d => y(d.orders))
+            .attr('fill', 'orange')
+            .attr('x', (d, i) => i * 70 + 10);
+
+    rects.exit().remove();
+});
---
+++ menu.js
@@ -0,0 +1,18 @@
+[
+    {
+        "name": "vegan soup",
+        "orders": 200
+    },
+    {
+        "name": "vegan curry",
+        "orders": 600
+    },
+    {
+        "name": "vegan pasta",
+        "orders": 300
+    },
+    {
+        "name": "vegan surprise",
+        "orders": 900
+    }
+]
