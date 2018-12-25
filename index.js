3f544ad497edf1063c7c53047311c7c2763f66a7
Rogelio <r.garcia11@uniandes.edu.co>
2018-12-25 12:42:46 -0500

    Section/4 (#3)
    * Class 19
    
    * Class 20
    
    * Class 22
    
    * Class 23
    
    * Class 25
    
    * Class 26
    
    * Class 28

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
 // select svg container first
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+    .append('svg')
+        .attr('width', 600)
+        .attr('height', 600);
 
-d3.json("planets.json").then(data => {
-    const circs = svg.selectAll('circle')
-        .data(data);
+// create margins and dimensions for the graph area
+const margin = {top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+// create axis groups
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append('g');
+
+d3.json('menu.js').then( data => {
+    // const min = d3.min(data, d => d.orders);
+    // const max = d3.max(data, d => d.orders);
+    // const extent = d3.extent(data, d => d.orders);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.orders)])
+        .range([graphHeight, 0]);
     
-    // add attrs to circs already in DOM (there are none, but its safer)
-    circs.attr('cy', 200)
-        .attr('cx', d => d.distance)
-        .attr('r', d => d.radius)
-        .attr('fill', d => d.fill);
+    const x = d3.scaleBand()
+        .domain(data.map( item => item.name ))
+        .range([0, 500])
+        .paddingInner(0.2)
+        .paddingOuter(0.2);
 
-    // append the enter selection to the DOM and add attributes
-    circs.enter()
-        .append('circle')
-            .attr('cy', 200)
-            .attr('cx', d => d.distance)
-            .attr('r', d => d.radius)
-            .attr('fill', d => d.fill);
-})
+    const rects = graph.selectAll('rect')
+        .data(data);
+
+    rects.attr('width', x.bandwidth)
+        .attr('height', d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
+
+    rects.enter()
+        .append('rect')
+            .attr('width', x.bandwidth)
+            .attr('height', d => graphHeight - y(d.orders))
+            .attr('fill', 'orange')
+            .attr('x', d => x(d.name))
+            .attr('y', d => y(d.orders));
+
+    rects.exit().remove();
+
+    // create and call the axis
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y)
+        .ticks(4)
+        .tickFormat(d => `${d} orders`);
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+    xAxisGroup.selectAll('text')
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end')
+        .attr('fill', 'orange');
+});
