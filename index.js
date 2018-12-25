5c8129ef600983ed873c8c4313328e6fb14ad652
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-25 14:36:58 -0500

    Class 40

--- index.js
+++ index.js
@@ -77,24 +77,30 @@ const update = data => {
 
 }
 
-db.collection('dishes').get().then( res => {
-
-    var data = [];
-    res.docs.forEach(doc => {
-        data.push(doc.data());
-    });
-
-    d3.interval(() => {
-        data.pop();
-        update(data);
-    }, 3000);
-
-});
-
 var data = [];
 
 db.collection('dishes').onSnapshot(res => {
-    res.docChanges().forEach(docChange => {
-        change.doc.data();
+    
+    res.docChanges().forEach(change => {
+        const doc = {...change.doc.data(), id: change.doc.id};
+
+        switch(change.type){
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id);
+                data.remove(doc);
+                break;
+            default:
+                break;
+        }        
     });
+
+    update(data);
+
 });
