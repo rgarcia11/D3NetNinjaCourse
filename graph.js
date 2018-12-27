7efb4ad2f5b62d6b2041729fbfac4282abce3361
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-27 01:42:48 -0500

    Class 53

--- graph.js
+++ graph.js
@@ -23,4 +23,37 @@ const arcPath = d3.arc()
     .outerRadius(dims.radius)
     .innerRadius(dims.radius / 2);
 
-graph.append('path').attr('d', arcPath(angles[0]))
+graph.append('path').attr('d', arcPath(angles[0]));
+
+const update = data => {
+    console.log(data);
+}
+
+let data = [];
+
+db.collection('expenses').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+
+        const doc = {...change.doc.data(), id: change.doc.id};
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id);
+                break;
+            default:
+                break;
+        }
+
+    });
+
+    update(data);
+
+});
