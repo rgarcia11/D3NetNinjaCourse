32b41c5a5ace4900a127db9629e82a73355bad06
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-28 17:34:21 -0500

    Class 87

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-const btns = document.querySelectorAll('button');
-const form = document.querySelector('form');
-const formAct = document.querySelector('form span');
-const input = document.querySelector('input');
-const label = document.querySelector('form label');
-const error = document.querySelector('.error');
-
-let activity = 'cycling';
-
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        activity = e.target.dataset.activity;
-
-        btns.forEach(btn => btn.classList.remove('active'));
-        e.target.classList.add('active');
-
-        input.setAttribute('id', activity);
-        label.htmlFor = activity;
-        formAct.textContent = activity;
-
-        update(data);
-    });
-});
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    const distance = parseInt(input.value);
-    if(distance){
-        db.collection('activities').add({
-            distance,
-            activity,
-            date: new Date().toString()
-        }).then(() => {
-            input.value = '';
-            error.textContent = '';
-        });
-    } else {
-        error.textContent = 'Please enter a valid distance!';
-    }
-});
+const data = [
+    { name: 'news', parent: '' },
+    { name: 'tech', parent: 'news' },
+    { name: 'sport', parent: 'news' },
+    { name: 'music', parent: 'news' },
+    { name: 'ai', parent: 'tech', amount: 7 },
+    { name: 'coding', parent: 'tech', amount: 5 },
+    { name: 'tablets', parent: 'tech', amount: 4 },
+    { name: 'laptops', parent: 'tech', amount: 6 },
+    { name: 'd3', parent: 'tech', amount: 3 },
+    { name: 'gaming', parent: 'tech', amount: 3 },
+    { name: 'football', parent: 'sport', amount: 6 },
+    { name: 'hockey', parent: 'sport', amount: 3 },
+    { name: 'baseball', parent: 'sport', amount: 5 },
+    { name: 'tennis', parent: 'sport', amount: 6 },
+    { name: 'f1', parent: 'sport', amount: 1 },
+    { name: 'house', parent: 'music', amount: 3 },
+    { name: 'rock', parent: 'music', amount: 2 },
+    { name: 'punk', parent: 'music', amount: 5 },
+    { name: 'jazz', parent: 'music', amount: 2 },
+    { name: 'pop', parent: 'music', amount: 3 },
+    { name: 'classical', parent: 'music', amount: 5 },
+  ];
+  
+  const stratify = d3.stratify()
+    .id(d => d.name)
+    .parentId(d => d.parent);
+  
+  console.log(stratify(data));
+  
