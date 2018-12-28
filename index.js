b4698618a5e131c16ae42d896a752cf7859c7a0b
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-28 00:39:35 -0500

    Class 71

---
+++ index.js
@@ -0,0 +1,37 @@
+const btns = document.querySelectorAll('button');
+const form = document.querySelector('form');
+const formAct = document.querySelector('form span');
+const input = document.querySelector('input');
+const error = document.querySelector('.error');
+
+let activity = 'cycling';
+
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        activity = e.target.dataset.activity;
+        
+        btns.forEach(btn => btn.classList.remove('active'));
+        e.target.classList.add('active');
+
+        input.setAttribute('id', activity);
+        formAct.textContent = activity;
+    });
+});
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const distance = parseInt(input.value);
+    if(distance){
+        db.collection('activities').add({
+            distance,
+            activity,
+            date: new Date().toString()
+        }).then(() => {
+            input.value = '';
+            error.textContent = '';
+        });
+    } else {
+        error.textContent = 'Please enter a valid distance!';
+    }
+});
