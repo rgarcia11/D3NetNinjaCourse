4545e6f7de46b7b67e18faad3f82a9b443167e7f
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-28 17:50:17 -0500

    Class 88

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const data = [
     { name: 'news', parent: '' },
+
     { name: 'tech', parent: 'news' },
     { name: 'sport', parent: 'news' },
     { name: 'music', parent: 'news' },
+
     { name: 'ai', parent: 'tech', amount: 7 },
     { name: 'coding', parent: 'tech', amount: 5 },
     { name: 'tablets', parent: 'tech', amount: 4 },
     { name: 'laptops', parent: 'tech', amount: 6 },
     { name: 'd3', parent: 'tech', amount: 3 },
     { name: 'gaming', parent: 'tech', amount: 3 },
+
     { name: 'football', parent: 'sport', amount: 6 },
     { name: 'hockey', parent: 'sport', amount: 3 },
     { name: 'baseball', parent: 'sport', amount: 5 },
     { name: 'tennis', parent: 'sport', amount: 6 },
     { name: 'f1', parent: 'sport', amount: 1 },
+
     { name: 'house', parent: 'music', amount: 3 },
     { name: 'rock', parent: 'music', amount: 2 },
     { name: 'punk', parent: 'music', amount: 5 },
@@ -22,9 +26,23 @@ const data = [
     { name: 'classical', parent: 'music', amount: 5 },
   ];
   
-  const stratify = d3.stratify()
+  const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', 1060)
+    .attr('height', 800);
+
+const graph = svg.append('g')
+  .attr('transform', `translate(50, 50)`);
+
+const stratify = d3.stratify()
     .id(d => d.name)
     .parentId(d => d.parent);
-  
-  console.log(stratify(data));
-  
+
+const rootNode = stratify(data)
+    .sum(d => d.amount);
+
+const pack = d3.pack()
+  .size([960, 700])
+  .padding(5);
+
+const bubbleData = pack(rootNode).descendants();
