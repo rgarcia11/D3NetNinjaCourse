5019c57d62dea6331c1f19b6fb1afa8954fd8a6e
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-12-29 01:45:47 -0500

    Class 94

--- graph.js
+++ graph.js
@@ -1,3 +1,13 @@
+const dims = { height: 500, width: 1100 };
+
+const svg = d3.select('.canvas')
+    .append('svg')
+    .width('width', dims.width + 100)
+    .height('height', dims.height + 100);
+
+const graph = svg.append('g')
+    .attr('transform', `translate(50, 50)`);
+
 let data = [];
 
 db.collection('members').onSnapshot(res => {
